848b69c27e33da01d00f039707147992361ebf41
宋江 <tianye263@gmail.com>
2017-01-09 09:21:37 +0800

    查看个人信息2

--- member.php
+++ member.php
@@ -6,7 +6,11 @@ include './common/common.php';
 if (!empty($_GET['q'])) {
 	$username = $_GET['q'];
 	$title = $username.'的详细信息';
-	$result = select($link , 'tt_user' , 'username,email,hicon,gragh,info' , "$username");
+	$result = select($link , 'tt_user' , 'username,email,hicon,gragh,info' , "username = '$username'");
 
+	display('home/member.html' , compact('result' , 'title'));
+} else {
+	$title = '详细信息';
+	$result = '';
 	display('home/member.html' , compact('result' , 'title'));
 }
