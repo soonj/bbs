a30dc813aee6c704c7da9bd87501a754b884d0ae
宋江 <tianye263@gmail.com>
2017-01-10 14:14:00 +0800

    多文件上传处理函数

--- helper/upload_func.php
+++ helper/upload_func.php
@@ -81,6 +81,22 @@ function upload($key, $mimeArray, $suffixArray, $maxSize, $path, $isRand)
 }
 
 
+function reArrayFiles(&$file_post) {
+
+    $file_ary = array();
+    $file_count = count($file_post['name']);
+    $file_keys = array_keys($file_post);
+
+    for ($i=0; $i<$file_count; $i++) {
+        foreach ($file_keys as $key) {
+            $file_ary[$i][$key] = $file_post[$key][$i];
+        }
+    }
+
+    return $file_ary;
+}
+
+
 
 
 
