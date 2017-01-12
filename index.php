a34f16f3541fa6465271880f3e375ab068b0b322
soonj <tianye263@gmail.com>
2017-01-13 01:25:26 +0800

    父子节点遍历

--- index.php
+++ index.php
@@ -18,16 +18,29 @@ if (!empty($_GET['nowPage'])) {
 	$nowPage = 0;
 	$offset = 0;
 }
-//----------分页查询主题信息
-if (!empty($_GET['tab']) && empty($_GET['tab'])) {
-	$sql = "SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from tt_user INNER JOIN tt_post ON uid = authorid WHERE {DB_PREFIX}post.rid = 0 AND {DB_PREFIX}post.display = 1 AND {DB_PREFIX}post.tabname = '{$_GET['tab']}' ORDER BY ctime DESC,retime DESC limit $offset,10";
-}elseif (!empty($_GET['go']) && !empty($_GET['tab'])) {
-	$sql = "SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from tt_user INNER JOIN tt_post ON uid = authorid WHERE {DB_PREFIX}post.rid = 0 AND {DB_PREFIX}post.display = 1 AND {DB_PREFIX}post.tabname = '{$_GET['tab']}' AND {DB_PREFIX}post.nodename = '{$_GET['go']}'tt_ORDER BY ctime DESC,retime DESC limit $offset,10";
+
+//--------查询节点信息
+
+$tab = select($link , DB_PREFIX.'tab' , '*' , "id > 0");
+if (!empty($_GET['tab'])) {
+	$tabname = $_GET['tab'];
+	$tab_result = select($link , DB_PREFIX.'tab' , '*' , "tabname = '$tabname'");
 }
-$sql = "SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from tt_user INNER JOIN tt_post ON uid = authorid WHERE {DB_PREFIX}post.rid = 0 AND {DB_PREFIX}post.display = 1 ORDER BY ctime DESC,retime DESC limit $offset,10";
+var_dump($tab_result);
+
+
+
+//----------分页查询主题信息
+if (!empty($_GET['tab'])) {
+	$sql = 'SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from '.DB_PREFIX.'user INNER JOIN tt_post ON uid = authorid INNER JOIN '.DB_PREFIX.'tab ON nodename = tabname WHERE '.DB_PREFIX.'post.rid = 0 AND '.DB_PREFIX.'post.display = 1 AND parentid = '.$tab_result[0]['id'].' ORDER BY ctime DESC,retime DESC limit '.$offset.',10';
+}elseif (!empty($_GET['go'])) {
+	$sql = 'SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from '.DB_PREFIX.'user INNER JOIN tt_post ON uid = authorid INNER JOIN '.DB_PREFIX.'tab ON nodename = tabname WHERE '.DB_PREFIX.'post.rid = 0 AND '.DB_PREFIX.'post.display = 1 ORDER BY ctime DESC,retime DESC limit '.$offset.',10';
+}elseif (empty($_GET['go']) && empty($_GET['tab'])) {
+	$sql = 'SELECT username as author_name,pid,hicon,title,rcount,lastuser,nodename,ctime,coin,retime from '.DB_PREFIX.'user INNER JOIN '.DB_PREFIX.'post ON uid = authorid WHERE '.DB_PREFIX.'post.rid = 0 AND '.DB_PREFIX.'post.display = 1 ORDER BY ctime DESC,retime DESC limit '.$offset.',10';
+}
 $result = mysqli_query($link,$sql);
 //----------主题页数查询赋值
-$count = select($link , 'tt_post' , 'count(pid)' , 'rid = 0 and display = 1');
+$count = mysqli_num_rows($result);
 $pageCount = $count[0]['count(pid)'] / 10;
 
 //----------遍历输出到数组
@@ -37,9 +50,9 @@ if ($result) {
 		$data[]=$row;
 	}
 }
+var_dump($sql);
 
-//--------查询父节点信息
-$tab = select($link , DB_PREFIX.'tab' , '*' , 'parentid >= 0');
+
 
 //模版引擎传值
 $title = 'TICK-TOCK'; 
@@ -47,8 +60,14 @@ $count = select($link , 'tt_post' , 'count(pid)' , 'rid = 0');
 $totalPage = ceil($pageCount);
 $baseUrl = WEB_SITE.'/index.php';
 $search = [];
+if (!empty($_GET['go'])) {
+	$search['go']= $_GET['go'];	
+}if (!empty($_GET['tab'])) {
+	$search['tab']= $_GET['tab'];	
+}
 
-display('home/index.html' , compact('data' , 'title' , 'totalPage' , 'baseUrl' , 'search' , 'nowPage'));
+
+display('home/index.html' , compact('data' , 'title' , 'totalPage' , 'baseUrl' , 'search' , 'nowPage' , 'tab' , 'tab_result'));
 
 //----------关闭数据库
 mysqli_close($link);
